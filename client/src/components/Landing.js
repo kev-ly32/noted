cdb7b54018b0a4769244c26e644914a3981c9b17
Kevin Ly <kev_ly32@hotmail.com>
2020-12-12 15:32:08 -0500

    updating landing carousel

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,26 +1,76 @@
 import React from "react";
-import { Container, Typography, Box, Grid } from "@material-ui/core";
+import {
+  Container,
+  Typography,
+  Box,
+  Grid,
+  Paper,
+  Button,
+} from "@material-ui/core";
+import Carousel from "react-material-ui-carousel";
+
+const LandingCarousel = () => {
+  const items = [
+    {
+      name: "Keep track of your tasks.",
+      description: "Anything you want, we'll have it Noted.",
+      image:
+        "https://res.cloudinary.com/de5gzocha/image/upload/v1607749681/Noted/LandingChecklist_sjkidn.svg",
+    },
+    {
+      name: "Slick dashboard for ease of use.",
+      description: "Spend less time planning, more time doing.",
+      image:
+        "https://res.cloudinary.com/de5gzocha/image/upload/v1607803727/Noted/Multitasking_dhereh.svg",
+    },
+  ];
+  return (
+    <Carousel interval={10000}>
+      {items.map((item, i) => (
+        <Item key={i} item={item} />
+      ))}
+    </Carousel>
+  );
+};
+
+const Item = (props) => {
+  return (
+    <Paper elevation={5}>
+      <Grid container style={{ paddingTop: "5%" }}>
+        <Box display="flex" alignItems="center">
+          <Grid item xs={6}>
+            <Box display="flex" flexDirection="column" alignItems="flex-end">
+              <h2>{props.item.name}</h2>
+              <p>{props.item.description}</p>
+              <Button
+                color="primary"
+                variant="outlined"
+                className="CheckButton"
+              >
+                Get Started
+              </Button>
+            </Box>
+          </Grid>
+          <Grid item xs={6}>
+            <Box display="flex" justifyContent="center">
+              <img
+                style={{ width: "60%", height: "300px" }}
+                src={props.item.image}
+                alt="Landing Checklist"
+              />
+            </Box>
+          </Grid>
+        </Box>
+      </Grid>
+    </Paper>
+  );
+};
 
 function Landing(props) {
   return (
     <Container maxWidth="lg">
-      <Typography component="div" style={{ height: "100vh" }}>
-        <Grid container style={{ paddingTop: "5%" }}>
-          <Box display="flex" alignItems="center">
-            <Grid item xs={6} style={{ padding: "0 5%" }}>
-              <h1>Keep track of your tasks. We'll have it Noted.</h1>
-            </Grid>
-            <Grid item xs={6}>
-              <Box display="flex" justifyContent="center">
-                <img
-                  style={{ width: "30em" }}
-                  src="https://res.cloudinary.com/de5gzocha/image/upload/v1607749681/Noted/LandingChecklist_sjkidn.svg"
-                  alt="Landing Checklist"
-                />
-              </Box>
-            </Grid>
-          </Box>
-        </Grid>
+      <Typography component="div" style={{ height: "80vh" }}>
+        <LandingCarousel />
       </Typography>
     </Container>
   );
